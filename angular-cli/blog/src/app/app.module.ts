4dad463fee5beca47572ecdeeb2151493c756399
RomanDG <romand85@yandex.ru>
2018-08-22 11:36:27 +0300

    lesson one

--- angular-cli/blog/src/app/app.module.ts
+++ angular-cli/blog/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './common-module/shared-components/posts/posts.component';
+import { PostComponent } from './common-module/shared-components/post/post.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    PostComponent
   ],
   imports: [
     BrowserModule
