3977263a956c78ce86ee3f8d8a9d4656b2d02f74
RomanDG <romand85@yandex.ru>
2018-08-29 18:52:07 +0300

    удалил ненужное

--- angular-cli/blog/src/app/app.module.ts
+++ angular-cli/blog/src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-//import { RenderableListComponent } from './common-module/shared-components/renderable-list/renderable-list.component';
 import { UserModule } from './user-module/user.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    //RenderableListComponent,
   ],
   imports: [
     BrowserModule,
