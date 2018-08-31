49e9eefd1a9025d513b8f9e304965202f934b86b
RomanDG <romand85@yandex.ru>
2018-08-31 10:14:09 +0300

    доделал директиву change-image

--- angular-cli/blog/src/app/user-module/directives/change-image.directive.ts
+++ angular-cli/blog/src/app/user-module/directives/change-image.directive.ts
@@ -1,10 +1,26 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input, Renderer2, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appChangeImage]'
 })
 export class ChangeImageDirective {
 
-  constructor(private elem: ElementRef) { }
+  public sizeImage = 1;
+  @Input() image: any;
+  @HostListener('click') onclick() {
+    switch (this.sizeImage) {
+      case 1:
+        this.renderer.setAttribute(this.elem.nativeElement, 'src', this.image.medium);
+        this.sizeImage = 2; break;
+      case 2:
+        this.renderer.setAttribute(this.elem.nativeElement, 'src', this.image.thumbnail);
+        this.sizeImage = 3; break;
+      case 3:
+        this.renderer.setAttribute(this.elem.nativeElement, 'src', this.image.large);
+        this.sizeImage = 1; break;
+    }
+  }
+
+  constructor(private elem: ElementRef, private renderer: Renderer2) { }
 
 }
